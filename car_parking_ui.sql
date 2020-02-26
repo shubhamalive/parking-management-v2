a78b74ae5ae20c8939c46c0a386b259840120715
Shubham Jain <shubhamalive@gmail.com>
2020-02-26 19:54:07 +0530

    Update car_parking_ui.sql

--- car_parking_ui.sql
+++ car_parking_ui.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2020 at 09:39 PM
+-- Generation Time: Feb 26, 2020 at 03:22 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -33,34 +33,35 @@ CREATE TABLE `parking_lot` (
   `name` varchar(10) NOT NULL,
   `parking_id` varchar(10) NOT NULL,
   `vehicle_no` varchar(20) NOT NULL,
-  `mobile_no` varchar(10) NOT NULL
+  `mobile_no` varchar(10) NOT NULL,
+  `date_date` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `parking_lot`
 --
 
-INSERT INTO `parking_lot` (`id`, `name`, `parking_id`, `vehicle_no`, `mobile_no`) VALUES
-(1, 'A1', '0', '', ''),
-(2, 'A2', '0', '', ''),
-(3, 'A3', '0', '', ''),
-(4, 'A4', '0', '', ''),
-(7, 'B1', '0', '', ''),
-(8, 'B2', '0', '', ''),
-(9, 'B3', '0', '', ''),
-(10, 'B4', '0', '', ''),
-(13, 'C1', '0', '', ''),
-(14, 'C2', '0', '', ''),
-(15, 'C3', '0', '', ''),
-(16, 'C4', '0', '', ''),
-(19, 'D1', '0', '', ''),
-(20, 'D2', '0', '', ''),
-(21, 'D3', '0', '', ''),
-(22, 'D4', '0', '', ''),
-(25, 'E1', '0', '', ''),
-(26, 'E2', '0', '', ''),
-(27, 'E3', '0', '', ''),
-(28, 'E4', '0', '', '');
+INSERT INTO `parking_lot` (`id`, `name`, `parking_id`, `vehicle_no`, `mobile_no`, `date_date`) VALUES
+(1, 'A1', '0', '', '', '2020-02-26 14:02:13'),
+(2, 'A2', '0', '', '', '2020-02-26 13:18:39'),
+(3, 'A3', '0', '', '', '2020-02-26 13:18:54'),
+(4, 'A4', '0', '', '', '2020-02-26 13:19:04'),
+(7, 'B1', '0', '', '', '2020-02-26 13:19:07'),
+(8, 'B2', '0', '', '', '2020-02-26 13:22:52'),
+(9, 'B3', '0', '', '', '2020-02-26 13:22:31'),
+(10, 'B4', '0', '', '', '2020-02-26 13:19:33'),
+(13, 'C1', '0', '', '', '2020-02-26 13:22:39'),
+(14, 'C2', '0', '', '', '2020-02-26 14:02:06'),
+(15, 'C3', '0', '', '', '2020-02-26 13:20:22'),
+(16, 'C4', '0', '', '', '2020-02-26 13:20:14'),
+(19, 'D1', '0', '', '', '2020-02-26 13:20:30'),
+(20, 'D2', '0', '', '', '2020-02-26 13:20:37'),
+(21, 'D3', '0', '', '', '2020-02-26 13:20:44'),
+(22, 'D4', '0', '', '', '2020-02-26 13:20:51'),
+(25, 'E1', '0', '', '', '2020-02-26 13:20:59'),
+(26, 'E2', '0', '', '', '2020-02-26 13:21:07'),
+(27, 'E3', '0', '', '', '2020-02-26 13:21:15'),
+(28, 'E4', '0', '', '', '2020-02-26 14:11:17');
 
 -- --------------------------------------------------------
 
@@ -106,7 +107,7 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT for table `parking_lot`
 --
 ALTER TABLE `parking_lot`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
